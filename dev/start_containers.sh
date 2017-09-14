8eea18fe01b76b635f1c976834afeb3e3235bba8
Bryan Lott <mystickphoenix@gmail.com>
2017-09-13 19:17:35 -0600

    docker compose, live flask edits work, neo4j works

--- dev/start_containers.sh
+++
@@ -1,4 +0,0 @@
-docker pull neo4j
-docker run -d --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data neo4j
-docker build -t level-15-dev .
-docker run -d --name level-15 -p 80:80 -v $(pwd)/app:/app level-15-dev
