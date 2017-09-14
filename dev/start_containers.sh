ad41aad39acd5e6283ad317ee2f75c1130cfb4af
Bryan Lott <mystickphoenix@gmail.com>
2017-09-13 18:55:41 -0600

    Update start_containers.sh

--- dev/start_containers.sh
+++ dev/start_containers.sh
@@ -1,2 +1,4 @@
 docker pull neo4j
-docker run --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data neo4j
+docker run --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data neo4j &
+docker build -t level-15-dev .
+docker run -d --name level-15 -p 80:80 level-15-dev
